d673da0460f86a549b70c8467c89bc3e8f6c6a39
TRAN TUAN <tran_trong_tuan@kanatec.jp>
2023-02-22 10:48:24 +0900

    DataStructure

---
+++ TestLeetCode/TestLeetCode/TestLeetCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestLeetCode
+{
+    public class Solution
+    {
+        //Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+        //Input: prices = [7,1,5,3,6,4]
+        //Output: 5
+        //Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+        //Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+        
+        public int MaxProfit(int[] prices)
+        {
+            var maxProfit = 0;
+            int leftPointer = 0;
+            int rightPointer = 1;
+            while (rightPointer < prices.Length)
+            {
+                if (prices[rightPointer] > prices[leftPointer])
+                {
+                    var profit = prices[rightPointer] - prices[leftPointer];
+                    maxProfit = Math.Max(profit, maxProfit);
+                }
+                else
+                {
+                    leftPointer = rightPointer;
+                }
+                    rightPointer++;
+            }
+            return maxProfit;
+        }
+    }
+}
